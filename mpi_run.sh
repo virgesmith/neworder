34fa74ca547ace50c75457757a260c22e79fa711
virgesmith <a.p.smith@leeds.ac.uk>
2018-08-08 15:50:14 +0100

    tweaks for HPC env/preserve PYTHONPATH

--- mpi_run.sh
+++ mpi_run.sh
@@ -1,1 +1,6 @@
-LD_LIBRARY_PATH=src/lib mpirun -n 2 src/bin/neworder_mpi examples/people
+#!/bin/bash
+
+# for ARC3 non-conda env
+PYTHONPATH=~/.local/lib/python3.6/site-packages:$PYTHONPATH
+
+LD_LIBRARY_PATH=src/lib:$LD_LIBRARY_PATH mpirun -n 8 src/bin/neworder_mpi examples/people
--- run_example.sh
+++ run_example.sh
@@ -5,4 +5,7 @@ if [ "$#" != "1" ]; then
   exit 1
 fi
 
+# for ARC3 non-conda env
+PYTHONPATH=~/.local/lib/python3.6/site-packages:$PYTHONPATH
+
 LD_LIBRARY_PATH=src/lib:$LD_LIBRARY_PATH src/bin/neworder examples/$1
