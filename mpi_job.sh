c0acfae8d4ea70205e5c74852a2b788ff272bb88
virgesmith <a.p.smith@leeds.ac.uk>
2018-08-22 09:45:42 +0100

    update mpi submission script

--- mpi_job.sh
+++ mpi_job.sh
@@ -1,18 +1,24 @@
-#!/bin/bash 
 
-##$ -l h_vmem=2G
-# Export variables and use current working directory 
 #$ -cwd -V 
-# Request 1 hour of runtime 
+# Request runtime 
 #$ -l h_rt=1:00:00 
-# Request 24 CPU cores (seems to be the max allowed) 
-#$ -pe ib 24 
+# Request CPU cores 
+#$ -pe ib 48 
+
+# Martin tip:
+##$ -l nodes=2 
+# gives me 2 CPUs = 48 cores, & all their memory
+
+#$ -m e
+#$ -M a.p.smith@leeds.ac.uk
 
 # for ARC3 non-conda env
-PYTHONPATH=~/.local/lib/python3.6/site-packages:$PYTHONPATH
+export PYTHONPATH=~/.local/lib/python3.6/site-packages:$PYTHONPATH
+export LD_LIBRARY_PATH=src/lib:$LD_LIBRARY_PATH 
 
-echo Start: $(date) > mpi_out.log
+#echo Start: $(date) > mpi_out.log
 
-LD_LIBRARY_PATH=src/lib:$LD_LIBRARY_PATH mpirun src/bin/neworder_mpi examples/people_big examples/shared >> mpi_out.log 2> mpi_err.log
+mpirun src/bin/neworder_mpi examples/people_big examples/shared >> mpi_out.log 2> mpi_err.log
 
-echo Finish: $(date) >> mpi_out.log
+#echo Finish: $(date) >> mpi_out.log
+
