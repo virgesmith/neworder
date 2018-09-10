8ee7572e96b3c9785dbd4f8a504d98f23dc5b99d
virgesmith <a.p.smith@leeds.ac.uk>
2018-09-10 11:49:53 +0100

    start household model

---
+++ examples/households/households.py
@@ -0,0 +1,21 @@
+""" households.py """
+
+import pandas as pd
+import neworder as no
+
+class Households:
+  def __init__(self, init_pop):
+    self.pop = pd.read_csv(init_pop)
+    #no.log(self.pop.columns.values)
+    no.log(self.pop.LC4408_C_AHTHUK11.unique())
+
+  def age(self, dt):
+
+    #LC4408_C_AHTHUK11
+    no.directmod(self.pop, "LC4408_C_AHTHUK11")
+
+  def check(self):
+    return True
+
+  def write_table(self):
+    no.log(self.pop.LC4408_C_AHTHUK11.unique())
