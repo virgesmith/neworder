f0352f847eaf800466289cb418650cc1008bdf24
virgesmith <a.p.smith@leeds.ac.uk>
2018-09-14 17:04:32 +0100

    move to C++14

--- src/lib/Log.h
+++ src/lib/Log.h
@@ -3,9 +3,22 @@
 
 #include "python.h"
 
+#include <string>
 #include <iostream>
 
+
 // C++14 implements the ""s -> std::string, use this for C++11
+// avoiding a using namespace decl in header results in a spurious warning (gcc bug) that we silence
+// warning: literal operator suffixes not preceded by ‘_’ are reserved for future standardization [-Wliteral-suffix]
+// see e.g. https://stackoverflow.com/questions/41444490/should-a-using-command-issue-a-warning-when-using-a-reserved-identifier
+// and https://stackoverflow.com/questions/41444135/how-to-make-stdoperators-visible-in-a-namespace?noredirect=1&lq=1
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wliteral-suffix"
+using std::literals::string_literals::operator""s;
+#pragma GCC diagnostic pop
+ 
+
+[[deprecated("use C++14  \"...\"s standard literal prefix")]]
 std::string operator ""_s(const char* p, size_t s);
 
 template<typename T>
@@ -45,25 +58,6 @@ std::string operator%(std::string&& str, T value)
   }
   return std::move(str);
 }
-
-// // need an rvalue ref as might/will be a temporary
-// template<typename T> 
-// std::string operator%(std::string&& str, const std::vector<T>& value)
-// {
-//   size_t s = str.find("%%");
-//   if (s != std::string::npos)
-//   {
-//     str.replace(s, 2, std::to_string(value[0])); 
-//   }
-//   return std::move(str);
-// }
-
-// // specialise for const char*
-// template<> 
-// std::string operator%(std::string&& str, const char* value);
-
-// // non-template for string (specialisation isn't matched for some reason)
-// std::string operator%(std::string&& str, const std::string& value);
 
 namespace neworder {
 
