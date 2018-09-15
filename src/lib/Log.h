5da9db49e997506f90075618e1c7cd8dcce85363
virgesmith <a.p.smith@leeds.ac.uk>
2018-09-15 13:03:07 +0100

    give up on c++14 std::string literal

--- src/lib/Log.h
+++ src/lib/Log.h
@@ -7,18 +7,9 @@
 #include <iostream>
 
 
-// C++14 implements the ""s -> std::string, use this for C++11
-// avoiding a using namespace decl in header results in a spurious warning (gcc bug) that we silence
-// warning: literal operator suffixes not preceded by ‘_’ are reserved for future standardization [-Wliteral-suffix]
-// see e.g. https://stackoverflow.com/questions/41444490/should-a-using-command-issue-a-warning-when-using-a-reserved-identifier
-// and https://stackoverflow.com/questions/41444135/how-to-make-stdoperators-visible-in-a-namespace?noredirect=1&lq=1
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wliteral-suffix"
-using std::literals::string_literals::operator""s;
-#pragma GCC diagnostic pop
- 
+// C++14 implements the ""s literal -> std::string but there are so many issues with it (namespace, gcc warnings)
+// just stick with the home-made version 
 
-[[deprecated("use C++14  \"...\"s standard literal prefix")]]
 std::string operator ""_s(const char* p, size_t s);
 
 template<typename T>
