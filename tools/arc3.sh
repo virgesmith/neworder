7738ddf1c9d380ebbcde22b542d1725457a7f163
virgesmith <a.p.smith@leeds.ac.uk>
2018-10-02 14:12:01 +0100

    closes #23

--- tools/arc3.sh
+++ tools/arc3.sh
@@ -2,8 +2,8 @@
 
 # make overrides for arc3
 
-echo make BOOST_EXTRA_CXXFLAGS=-I../../../boost_1_67_0/ \
-          BOOST_EXTRA_LDFLAGS=-L../../src/lib \
-          BOOST_PYTHON_LIB=boost_python36 \
-          BOOST_NUMPY_LIB=boost_numpy36 \
-          $@
+make BOOST_EXTRA_CXXFLAGS=-I../../../boost_1_67_0/ \
+     BOOST_EXTRA_LDFLAGS=-L../../src/lib \
+     BOOST_PYTHON_LIB=boost_python36 \
+     BOOST_NUMPY_LIB=boost_numpy36 \
+     $@
