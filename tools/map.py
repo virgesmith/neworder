eefeb6c1afe02fa0db29553e64dd95a91ce6d3e8
virgesmith <a.p.smith@leeds.ac.uk>
2018-10-09 17:48:05 +0100

    more map tweaks

--- tools/map.py
+++ tools/map.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 """
 Example of how to draw a map from a shapefile (or part thereof)
 """
 
-import geopandas 
 import pandas as pd
+import geopandas 
 import matplotlib.pyplot as plt
 import contextily as ctx
 from shapely.geometry import Point
@@ -22,20 +23,25 @@ def add_basemap(ax, crs, url='http://a.tile.stamen.com/terrain/tileZ/tileX/tileY
 def main(lad_match):
   # 4326 is lat/lon
   # 3857 is mercator
-  # see also /mnt/16.04/home/az/.ukboundaries/cache/
-  df = geopandas.read_file("../Mistral/data/england_msoa_2011.shp")
-  df = df[df.label.str.contains(lad_match)].to_crs(epsg=3857)
+  df = geopandas.read_file("../Mistral/data/Local_Authority_Districts_December_2016_Ultra_Generalised_Clipped_Boundaries_in_Great_Britain.shp")
+  print(df.columns.values)
+  df = df[df.lad16cd.str.contains(lad_match)].to_crs(epsg=3857)
+
+  df["unoblongity"] = df.st_areasha / df.st_lengths ** 2
 
   if not len(df):
     print("No polygons found")
     return
 
-  ax = df.plot(figsize=(10, 10), alpha=0.3, edgecolor='k')
-  # url="https://a.basemaps.cartocdn.com/light_all/tileZ/tileX/tileY.png"
+
+  ax = df.plot(figsize=(10, 10), alpha=0.5, column='unoblongity', cmap='Greens', edgecolor='k', ax=None)
+  # url="https://a.basemaps.cartocdn.com/light_all/tileZ/tileX/tileY.png" requires attribution: "Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL."
   # url="https://a.tile.openstreetmap.org/tileZ/tileX/tileY.png"
-  add_basemap(ax, df.crs) #, 
+  add_basemap(ax, df.crs, url="https://tiles.wmflabs.org/bw-mapnik/tileZ/tileX/tileY.png")
+  plt.title("GB LADs")
+  plt.axis("off")
   plt.show()
 
 if __name__ == "__main__":
-  lad_match = "E08000032|E18000035"
+  lad_match = ".*" #"E0|W0|S1"
   main(lad_match)
