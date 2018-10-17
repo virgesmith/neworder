95a64e10e34664ecefba03fb855f1f01db1eace3
virgesmith <a.p.smith@leeds.ac.uk>
2018-10-17 11:07:11 +0100

    add pyramid plot

--- examples/shared/pyramid.py
+++ examples/shared/pyramid.py
@@ -7,11 +7,24 @@ import matplotlib.pyplot as plt
 # see https://stackoverflow.com/questions/27694221/using-python-libraries-to-plot-two-horizontal-bar-charts-sharing-same-y-axis
 def plot(ages, males, females):
 
+  xmax = max(max(males), max(females))
+
   fig, axes = plt.subplots(ncols=2, sharey=True)
-  plt.gca().set_ylim([min(ages),max(ages)])
+  plt.gca().set_ylim([min(ages),max(ages)+1])
   axes[0].barh(ages, males, align='center', color='blue')
+  axes[0].set(title='Males')
+  axes[0].set(xlim=[0, xmax])
   axes[1].barh(ages, females, align='center', color='red')
+  axes[1].set(title='Females')
+  axes[1].set(xlim=[0, xmax])
+  axes[0].yaxis.tick_right()
+  #axes[1].set(yticks=ages)
   axes[0].invert_xaxis()
+  for ax in axes.flat:
+      ax.margins(0.03)
+      ax.grid(True)  
+  fig.tight_layout()
+  fig.subplots_adjust(wspace=0.125)
   plt.show()
 
 
