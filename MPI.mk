32104880ff91a9daaa498879f7fa4eb2a921fabc
virgesmith <a.p.smith@leeds.ac.uk>
2019-03-28 08:53:21 +0000

    fixes

--- MPI.mk
+++ MPI.mk
@@ -11,8 +11,11 @@ MPI_DEP := 0
 
 # Query python env for compile and link settings
 CXXFLAGS = $(shell python3 -m pybind11 --includes)
-CXXFLAGS += -O2 -Werror -Wno-error=deprecated-declarations -fPIC -std=c++14 -pedantic 
-CXXFLAGS += -DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION -DNEWORDER_MPI
+CXXFLAGS += -O2 -Werror -Wno-error=deprecated-declarations -fPIC -std=c++14 -pedantic -DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION -DNEWORDER_MPI
+# get version from __init__.py
+CXXFLAGS += -DNEWORDER_VERSION_MAJOR=$(shell python3 -c "import neworder;print(neworder.__version__.split('.')[0])") \
+            -DNEWORDER_VERSION_MINOR=$(shell python3 -c "import neworder;print(neworder.__version__.split('.')[1])") \
+            -DNEWORDER_VERSION_PATCH=$(shell python3 -c "import neworder;print(neworder.__version__.split('.')[2])")
 LDFLAGS := $(shell python$(PYVER)-config --ldflags)
 
 export
