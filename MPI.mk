d1ae7b99a3f7e880e1232163464383f744399a39
virgesmith <andrew@friarswood.net>
2020-08-06 14:22:46 +0100

    tweaks

--- MPI.mk
+++ MPI.mk
@@ -13,9 +13,14 @@ $(info Building in virtualenv: $(VIRTUAL_ENV))
 endif
 
 # python3.8 on ubuntu 20.04 (but not on travis) needs an extra arg "--embed" to resolve lib deps correctly, but this arg breaks previous versions
-PY_CFG_EXTRA_ARG =
-#PY_CFG_EXTRA_ARG += --embed
-PY_CFG=python-config
+# python3.8 on ubuntu 20.04 (but not on travis) needs an extra arg "--embed" to resolve lib deps correctly, but this arg breaks previous versions
+PY_CFG=python3-config
+# might need to be "python-config", and
+PY_CFG_LINK_ARG=--embed
+# might need to be "" on different platforms, override as necessary, e.g. make PY_CFG=python-config PY_CFG_LINK_ARG=
+
+$(info PY_CFG=$(PY_CFG))
+$(info PY_CFG_LINK_ARG=$(PY_CFG_LINK_ARG))
 
 # also set (CXXFLAGS below) NEWORDER_MPI to prevent skipping of MPI-specific code
 CXX=mpic++
@@ -33,7 +38,7 @@ CXXFLAGS += -Werror -Wno-error=deprecated-declarations -fPIC -std=c++17 -pedanti
 CXXFLAGS += -DNEWORDER_VERSION_MAJOR=$(shell python3 -c "import neworder;print(neworder.__version__.split('.')[0])") \
             -DNEWORDER_VERSION_MINOR=$(shell python3 -c "import neworder;print(neworder.__version__.split('.')[1])") \
             -DNEWORDER_VERSION_PATCH=$(shell python3 -c "import neworder;print(neworder.__version__.split('.')[2])")
-LDFLAGS := $(shell $(PY_CFG) --ldflags $(PY_CFG_EXTRA_ARG))
+LDFLAGS := $(shell $(PY_CFG) --ldflags $(PY_CFG_LINK_ARG))
 
 export
 
