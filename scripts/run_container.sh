72af0f7b705e94bcc70722fb9b6e11d8888561cc
virgesmith <andrew@friarswood.net>
2020-09-27 12:05:25 +0100

    missed stuff

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# on host system run `xhost +` got grant permission to connect to X
+# on host system run `xhost +` to grant permission to connect to X
 
 docker run -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -it virgesmith/neworder
 
