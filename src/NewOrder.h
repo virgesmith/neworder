9d2c327fd678e4e47f89910a0c13ded0d1ad083f
virgesmith <andrew@friarswood.net>
2020-10-22 10:05:03 +0100

    silence stl dll export warnings

--- src/NewOrder.h
+++ src/NewOrder.h
@@ -9,6 +9,8 @@ namespace py = pybind11;
 #if !defined(NEWORDER_EXPORT)
   #if defined(_MSC_VER)
     #define NEWORDER_EXPORT __declspec(dllexport)
+    // silence dll export warnings for STL objects
+    #pragma warning(disable:4521)
   #else
     #define NEWORDER_EXPORT __attribute__ ((visibility("default")))
   #endif
