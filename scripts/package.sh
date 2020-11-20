f63b0eb649f514dd2c7a694faf5466be56775afb
Andrew Smith <andrew@friarswood.net>
2020-11-20 12:08:29 +0000

    fix packaging issue

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-version=$(cat VERSION) 
+version=$(cat VERSION)
 
 # package as source dist
-python setup.py sdist 
+python setup.py sdist
 # upload
 #twine upload --repository-url https://test.pypi.org/legacy/ dist/neworder-$version.tar.gz
-twine upload --repository-url https://upload.pypi.org/legacy/ dist/neworder-$version*
+twine upload --repository-url https://upload.pypi.org/legacy/ dist/neworder-$version.tar.gz
 
