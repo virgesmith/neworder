e6df05891dc772e8695f87364159d3b2248fabe0
virgesmith <andrew@friarswood.net>
2021-01-14 18:31:50 +0000

    ?

--- scripts/create-conda-env.sh
+++ scripts/create-conda-env.sh
@@ -6,7 +6,6 @@ wget -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux
 bash miniconda.sh -b -p ./miniconda
 export PATH="./miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
-conda config --add channels conda-forge
 conda update -q conda
 conda install -q conda-build
 conda create -q -n conda-env python=$PYTHON
