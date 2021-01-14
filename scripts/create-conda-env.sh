e89fd891191b3c6174800120fbb6f33157cbb6d6
virgesmith <andrew@friarswood.net>
2021-01-14 17:49:28 +0000

    ...

--- scripts/create-conda-env.sh
+++ scripts/create-conda-env.sh
@@ -3,12 +3,13 @@
 # For use in CI scripts for conda builds. PYTHON env var must be set, to e.g. "3.8"
 
 wget -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
+bash miniconda.sh -b -p ./miniconda
+export PATH="./miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels conda-forge
 conda update -q conda
 conda install -q conda-build
 conda create -q -n conda-env python=$PYTHON
+conda init bash
 conda activate conda-env
 conda install gxx_linux-64 mpich numpy pandas pybind11 pytest mpi4py
