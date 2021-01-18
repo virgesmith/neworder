463c517580e3756a43dc50134e21a5a70ed05108
virgesmith <andrew@friarswood.net>
2021-01-18 10:50:20 +0000

    0.2.0 release

--- scripts/package.sh
+++ scripts/package.sh
@@ -5,8 +5,8 @@ version=$(cat VERSION)
 # package as source dist
 python setup.py sdist
 # upload
-twine upload --repository-url https://test.pypi.org/legacy/ dist/neworder-$version.tar.gz
-#twine upload --repository-url https://upload.pypi.org/legacy/ dist/neworder-$version.tar.gz
+#twine upload --repository-url https://test.pypi.org/legacy/ dist/neworder-$version.tar.gz
+twine upload --repository-url https://upload.pypi.org/legacy/ dist/neworder-$version.tar.gz
 
 # NB on testPyPI, deps need to been installed from main repo. Use this:
 #pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple
