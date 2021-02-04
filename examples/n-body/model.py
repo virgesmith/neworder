7b841e6cd3cf4215bf623dbba24c4657e399620f
virgesmith <andrew@friarswood.net>
2021-02-04 23:01:25 +0000

    update n-body

--- examples/n-body/model.py
+++ examples/n-body/model.py
@@ -4,7 +4,7 @@ import neworder as no
 from n_body import NBody
 
 G = 0.01
-N = 20
+N = 50
 dt = 0.01
 
 m = NBody(N, G, dt)
--- examples/n-body/n_body.py
+++ examples/n-body/n_body.py
@@ -3,16 +3,12 @@ import numpy as np
 import pandas as pd
 import neworder as no
 import matplotlib.pyplot as plt
-import seaborn as sns
-
-plt.style.use("dark_background")
 
 def _plot(bodies):
   plt.cla()
-  plt.scatter(bodies.x, bodies.y, s=bodies.m * 20)
-  plt.xlim(-0.2,1.2)
-  plt.ylim(-0.2,1.2)
-
+  plt.scatter(bodies.x, bodies.y, color="r", s=bodies.m * 20)
+  plt.xlim(-0.6,0.6)
+  plt.ylim(-0.6,0.6)
 
 class NBody(no.Model):
 
@@ -25,51 +21,62 @@ class NBody(no.Model):
     x_max = 1.0
     y_max = 1.0
 
+    #
+    r = self.mc().ustream(N) - 0.5
+    theta = self.mc().ustream(N) * np.pi
+    x = r * np.cos(theta) * x_max
+    y = r * np.sin(theta) * y_max
+
     self.bodies = pd.DataFrame(index=no.df.unique_index(N), data={
-        # "m": [0.1,1],
-        # "x": [0.3, 0.5],
-        # "y": [0.5, 0.5],
+        # "m": [10.0,0.1],
+        # "x": [0.0, 0.5],
+        # "y": [0.0, 0.0],
         # "vx": [0.0, 0.0],
-        # "vy": [0.0, 0.0],
+        # "vy": [0.0, 0.25],
         "m": self.mc().ustream(N) * m_max,
-        "x": self.mc().ustream(N) * x_max,
-        "y": self.mc().ustream(N) * y_max,
-        "vx": 0.0, # 1 * (self.mc().ustream(N) - 0.5),
-        "vy": 0.0, #1 * (self.mc().ustream(N) - 0.5),
+        "x": x,
+        "y": y,
+        # create angular momentum
+        "vx": -y,
+        "vy": x,
         "ax": np.nan,
         "ay": np.nan,
+        "ke": np.nan,
         "pe": np.nan
       })
 
+    # balance momentum by changing mass and velocity of body 0
+    self.bodies.m[0] *= 2
+    self.bodies.vx[0] -= (self.bodies.m * self.bodies.vx).sum() / self.bodies.m[0]
+    self.bodies.vy[0] -= (self.bodies.m * self.bodies.vy).sum() / self.bodies.m[0]
+
     self.__calc_a()
     #self.bodies.to_csv("bodies0.csv")
-    self.E0 = np.sum(self.bodies.pe) # assumes no KE
+    self.E0 = np.sum(self.bodies.pe + self.bodies.ke)  # assumes no KE
 
     _plot(self.bodies)
     plt.gca().set_aspect('equal', adjustable='box')
+    plt.gca().xaxis.set_visible(False)
+    plt.gca().yaxis.set_visible(False)
 
     plt.pause(0.001)
-    #self.debug = pd.DataFrame()
 
+  # also calc energy of system
   def __calc_a(self):
 
     for _,b in self.bodies.iterrows():
       dist2s = (self.bodies.x.values - b.x) ** 2 + (self.bodies.y.values - b.y) ** 2
       # first remove self-interactions
       dist2s = np.where(dist2s == 0.0, np.inf, dist2s)
-      # now avoid problems where particles too close
+      # fudge to avoid acceleration spikes when particles too close
       dist2s += .01
-      # TODO avoid massive accelerations
       dists = np.sqrt(dist2s)
-      #dists = np.maximum(np.sqrt(dist2s), 0.1)
       xhat = (self.bodies.x.values - b.x) / dists
       yhat = (self.bodies.y.values - b.y) / dists
-      #no.log(xhat**2 + yhat**2)
       b.ax = self.G * np.sum(self.bodies.m * xhat / dist2s)
       b.ay = self.G * np.sum(self.bodies.m * yhat / dist2s)
+      b.ke = b.m * (b.vx ** 2 + b.vy ** 2)
       b.pe = -self.G * b.m * np.sum(self.bodies.m / dists)
-
-    #self.debug = self.debug.append(self.bodies[["x", "y", "vx", "vy", "ax", "ay"]].loc[0])
 
   def __update_pos(self):
     self.bodies.x += self.bodies.vx * self.dt
@@ -84,27 +91,22 @@ class NBody(no.Model):
     # check momentum and energy conservation
     px = np.sum(self.bodies.m * self.bodies.vx)
     py = np.sum(self.bodies.m * self.bodies.vy)
-    ke = (self.bodies.m * (self.bodies.vx ** 2 + self.bodies.vy ** 2)).sum() / 2
+    ke = np.sum(self.bodies.ke)
     pe = np.sum(self.bodies.pe)
     no.log("p=%g,%g" % (px, py))
     no.log("e=%f" % (ke+pe-self.E0))
 
-    return True #np.fabs(ke+pe) < 1
+    return np.fabs(ke+pe-self.E0) < 20.2
 
   def step(self):
+    # 2nd order accurate, see https://medium.com/swlh/create-your-own-n-body-simulation-with-python-f417234885e9
     self.__update_v(0.5)
     self.__update_pos()
     self.__calc_a()
     self.__update_v(0.5)
 
-    #no.log(self.bodies)
-    #self.bodies.to_csv("bodies1.csv")
-    #self.halt()
-
-    #no.log(self.bodies)
     _plot(self.bodies)
     plt.pause(0.001)
 
   def finalise(self):
-    #self.debug.to_csv("debug.csv")
     pass
