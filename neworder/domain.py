5ac2899917658ee6506c1d4af8caafa9f34afb72
virgesmith <andrew@friarswood.net>
2021-06-09 15:56:46 +0100

    get space domain working

--- neworder/domain.py
+++ neworder/domain.py
@@ -1,16 +1,22 @@
 from os import stat
 import numpy as np
 
+def _bounce(point, min, max):
+  for d in range(len(point)):
+    if point[d] < min[d]: point[d] = min[d] - point[d]
+    if point[d] > max[d]: point[d] = max[d] - point[d]
+  return point
+
 class Domain:
 
   # Edge behaviour
+  UNBOUNDED = 0
   WRAP = 1
   CONSTRAIN = 2
-  #BOUNCE = 3
+  BOUNCE = 3
 
   def __init__(self, dim, edge, continuous):
     self.__dim = dim
-    assert edge in [Domain.WRAP, Domain.CONSTRAIN]
     self.__edge = edge
     self.__continuous = continuous
 
@@ -27,16 +33,20 @@ class Domain:
     return self.__continuous
 
 class Space(Domain):
+  """ Continuous rectangular n-dimensional domain """
 
-  @classmethod
-  def unbounded(cls, dim):
-    assert dim 
-    return cls(np.full(dim, -np.inf), np.full(dim, +np.inf))
+  @staticmethod
+  def unbounded(dim):
+    assert dim
+    return Space(np.full(dim, -np.inf), np.full(dim, +np.inf), edge=Domain.UNBOUNDED)
 
-  def __init__(self, min, max, edge = Domain.CONSTRAIN):
+  def __init__(self, min, max, edge=Domain.CONSTRAIN):
     assert len(min) and len(min) == len(max)
-    super().__init__(len(min), edge, True)
-    #self.dim = len(min)
+    super().__init__(len(min), edge, edge)
+
+    # Space supports all edge behaviours
+    assert edge in [Domain.UNBOUNDED, Domain.WRAP, Domain.CONSTRAIN, Domain.BOUNCE]
+
     self.min = min
     self.max = max
 
@@ -44,44 +54,83 @@ class Space(Domain):
   def extent(self):
     return self.min, self.max
 
-  def move(self, points, delta, ungroup=False):
+  def move(self, positions, velocities, delta_t, ungroup=False):
+    """ Returns translated positions AND velocities """
     # group tuples into a single array if necessary
-    if type(points) == tuple:
-      points = np.column_stack(points)
-    if type(delta) == tuple:
-      delta = np.column_stack(delta)
+    if type(positions) == tuple:
+      positions = np.column_stack(positions)
+    if type(velocities) == tuple:
+      velocities = np.column_stack(velocities)
 
-    assert points.dtype == float
-    assert delta.dtype == float
-    assert points.shape[-1] == self.dim and delta.shape[-1] == self.dim
-    if self.edge == Domain.CONSTRAIN:
-      result = np.clip(points + delta, self.min, self.max)
+    assert positions.dtype == float
+    assert velocities.dtype == float
+    assert positions.shape[-1] == self.dim and velocities.shape[-1] == self.dim
+    if self.edge == Domain.UNBOUNDED: 
+      p = positions + velocities * delta_t
+      v = velocities
+    elif self.edge == Domain.CONSTRAIN: 
+      p = positions + velocities * delta_t
+      v = velocities
+      hitmin = np.where(p < self.min, 1, 0)
+      p = np.where(hitmin, self.min, p)
+      v = np.where(hitmin, 0, v)
+      hitmax = np.where(p > self.max, 1, 0)
+      p = np.where(hitmax, self.max, p)
+      v = np.where(hitmax, 0, v)   
+    elif self.edge == Domain.BOUNCE:
+      p = positions + velocities * delta_t
+      v = velocities
+      hitmin = np.where(p < self.min, 1, 0)
+      p = np.where(hitmin, 2*self.min - p, p)
+      v = np.where(hitmin, -v, v)
+      hitmax = np.where(p > self.max, 1, 0)
+      p = np.where(hitmax, 2*self.max - p, p)
+      v = np.where(hitmax, -v, v)   
     else:
-      result = self.min + np.mod(points + delta - self.min, self.max - self.min)
+      p = self.min + np.mod(positions + velocities * delta_t - self.min, self.max - self.min)
+      v = velocities
 
     if ungroup:
-      result = np.split(result, self.dim, axis=1)
-    return result
+      p = np.split(p, self.dim, axis=1)
+      v = np.split(v, self.dim, axis=1)
+    return p, v
 
-  def dists2(self, points):
+  def dists2(self, points, to_points):
     # group tuples into a single array if necessary
     if type(points) == tuple:
       points = np.column_stack(points)
+    if type(to_points) == tuple:
+      to_points = np.column_stack(to_points)
+    # distances w.r.t. self if to_points not explicitly specified
+    if to_points is None:
+      to_points = points
     assert points.dtype == float
+    assert to_points.dtype == float
     n = points.shape[0]
+    m = to_points.shape[0]
     d = points.shape[1]
-    d2 = np.zeros((n,n))
-    for i in range(d):
-      d2 += (np.tile(points[:,i],n).reshape((n,n)) - np.repeat(points[:,i],n).reshape(n,n))**2
+    d2 = np.zeros((m,n))
+    if self.edge != Domain.WRAP:
+      for i in range(d):
+        d2 += (np.tile(points[:,i],m).reshape((m,n)) - np.repeat(to_points[:,i],n).reshape(m,n))**2
+    else: # wrapped domains need special treatment - distance across an edge may be less than naive distance
+      for i in range(d):
+        d1d = np.abs(np.tile(points[:,i],m).reshape((m,n)) - np.repeat(to_points[:,i],n).reshape(m,n))
+        r = self.max[i] - self.min[i]
+        d1d = np.where(d1d > r/2, r - d1d, d1d)
+        d2 += d1d*d1d
+
     return d2
 
-  def dists(self, points):
-    return np.sqrt(self.dists2(points))
+  def dists(self, points, to_points=None):
+    return np.sqrt(self.dists2(points, to_points))
 
   def __repr__(self):
     return "%s dim=%d min=%s max=%s edge=%s" % (self.__class__.__name__, self.dim, self.min, self.max, self.edge)
 
 class Grid(Domain):
+  """ Discrete rectangular n-dimensional domain """
+
   def __init__(self, ext, edge = Domain.CONSTRAIN):
     assert len(ext) and ext.dtype == np.int64
     super().__init__(len(ext), edge, False)
---
+++ test_domain.py
@@ -0,0 +1,60 @@
+
+from neworder.domain import Domain
+import numpy as np
+import neworder as no
+#import pandas as pd
+import matplotlib.pyplot as plt
+from time import sleep
+
+class SpaceTest(no.Model):
+
+  def __init__(self, n, edge):
+    super().__init__(no.LinearTimeline(0, 100), no.MonteCarlo.deterministic_identical_stream)
+
+    self.space2d = no.Space(np.array([-1.0, -3.0]), np.array([2.0, 5.0]), edge)
+
+    self.positions = self.mc().ustream(n * 2).reshape(n, 2)
+    self.velocities = (self.mc().ustream(n * 2) - 0.5).reshape(n, 2) 
+
+    self.fig, self.g = self.__init_visualisation()
+
+
+  def __init_visualisation(self):
+
+    plt.ion()
+
+    fig = plt.figure(constrained_layout=True, figsize=(8,8))
+    g = plt.scatter(self.positions[:,0], self.positions[:,1], s=40)
+    plt.xlim(self.space2d.min[0], self.space2d.max[0])
+    plt.ylim(self.space2d.min[1], self.space2d.max[1])
+    #plt.axis("off")
+
+    fig.canvas.mpl_connect('key_press_event', lambda event: self.halt() if event.key == "q" else None)
+
+    fig.canvas.flush_events()
+
+    return fig, g
+
+  def __update_visualisation(self):
+
+    self.g.set_offsets(np.c_[self.positions[:,0], self.positions[:,1]])
+    self.fig.canvas.flush_events()
+
+  def step(self):
+    self.positions, self.velocities = self.space2d.move(self.positions, self.velocities, 0.1)
+
+    self.__update_visualisation()
+    sleep(0.01)
+    #self.halt()
+
+  def check(self):
+    if self.space2d.edge == Domain.WRAP:
+      d = (self.space2d.max - self.space2d.min)/2
+      no.log(d@d)
+      return np.all(self.space2d.dists2(self.positions, self.positions) < d@d)
+    return True
+
+
+if __name__ == "__main__":
+  m = SpaceTest(300, Domain.WRAP)
+  no.run(m)
