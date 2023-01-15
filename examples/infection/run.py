ef340ec61ad9d3e5de45a968e0e5a3533f409432
virgesmith <andrew@friarswood.net>
2023-01-15 16:34:45 +0000

    add death

--- examples/infection/run.py
+++ examples/infection/run.py
@@ -10,6 +10,10 @@ I = 10
 speed = 10
 # max distance an infection can occur
 infection_radius = 10
+# number of steps infected before immunity
+recovery_time = 100
+# probability of dying from infection at any point during the infection
+mortality = 0.1
 
-m = Infection(N, I, speed, infection_radius)
+m = Infection(N, I, speed, infection_radius, recovery_time, mortality)
 no.run(m)
