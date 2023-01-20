4b19f26027ec054421a9d472cab580323c5a27ad
virgesmith <andrew@friarswood.net>
2023-01-20 19:57:35 +0000

    add Geospatial graph, improve movement

--- examples/infection/run.py
+++ examples/infection/run.py
@@ -5,15 +5,15 @@ from infection import Infection
 # number of agents
 N = 1000
 # initial number of infected agents
-I = 10
-# speed of movement
+I = 1
+# mean speed of movement
 speed = 10
 # max distance an infection can occur
-infection_radius = 10
+infection_radius = 1
 # number of steps infected before immunity
 recovery_time = 100
 # probability of dying from infection at any point during the infection
-mortality = 0.1
+mortality = 0.01
 
 m = Infection(N, I, speed, infection_radius, recovery_time, mortality)
 no.run(m)
