15b6cf9dfb1cf998aeaffdff6aebec0442b3fded
virgesmith <andrew@friarswood.net>
2023-03-30 20:02:01 +0100

    fix boids; remove old CI

--- examples/boids/boids2d.py
+++ examples/boids/boids2d.py
@@ -1,14 +1,21 @@
 from __future__ import annotations
 from typing import Any
+from time import sleep
 import numpy as np
 import pandas as pd # type: ignore
 import neworder as no
 import matplotlib.pyplot as plt # type: ignore
 
-TWO_PI = 2 * np.pi
-
 
 class Boids(no.Model):
+
+  ALIGN_COEFF = 2.0
+  COHERE_COEFF = 0.3
+  SEPARATE_COEFF = 0.5
+
+    # 'cohesion_strength': 0.005,
+    # 'seperation_strength': 0.1,
+    # 'alignment_strength': 0.3,
 
   def __init__(self, N: int, range: float, vision: float, exclusion: float, speed: float) -> None:
     super().__init__(no.LinearTimeline(0.0, 0.01), no.MonteCarlo.nondeterministic_stream)
@@ -22,25 +29,57 @@ class Boids(no.Model):
     # continuous wraparound 2d space
     self.domain = no.Space(np.zeros(2), np.full(2, self.range), edge=no.Edge.WRAP)
 
-    # maximum turns for each interaction
-    self.sep_max_turn = TWO_PI / 240 # 1.5 degrees
-    self.align_max_turn = TWO_PI / 72 # 5 degrees
-    self.cohere_max_turn = TWO_PI / 120 # 3 degrees
-
     # initially in [0,1]^dim
     self.boids = pd.DataFrame(
       index=pd.Index(name="id", data=no.df.unique_index(self.N)),
       data={
         "x": self.mc.ustream(N) * self.range,
         "y": self.mc.ustream(N) * self.range,
-        "theta": self.mc.ustream(N) * TWO_PI # zero is +ve x axis
+        "vx": self.mc.ustream(N) - 0.5,
+        "vy": self.mc.ustream(N) - 0.5,
       }
     )
+
+    self.__normalise_v()
 
     self.fig, self.g = self.__init_visualisation()
 
     # suppress divsion by zero warnings
     np.seterr(divide='ignore')
+
+  @classmethod
+  def test1(cls) -> None:
+
+    model = cls.__new__(cls)
+    super(cls, model).__init__(no.LinearTimeline(0.0, 0.01), no.MonteCarlo.nondeterministic_stream)
+
+    model.N = 3
+    model.range = 1.0
+    model.vision = 0.5
+    model.exclusion = 0.05
+    model.speed = 0.2
+
+    # continuous wraparound 2d space
+    model.domain = no.Space(np.zeros(2), np.full(2, 1.0), edge=no.Edge.WRAP)
+
+    model.boids = pd.DataFrame(
+      index=pd.Index(name="id", data=no.df.unique_index(model.N)),
+      data={
+        "x": [0.49, 0.5, 0.51],
+        "y": [0.0, 1.0, 0.0],
+        "vx": [0., 0., 0.],
+        "vy": [1., -1., 1.],
+      }
+    )
+
+    # model.N = 2
+    # model.boids = model.boids[:-1]
+
+    model.__normalise_v()
+
+    model.fig, model.g = model.__init_visualisation()
+    return model
+
 
   def step(self) -> None:
     d2, (dx, dy) = self.domain.dists2((self.boids.x, self.boids.y))
@@ -50,64 +89,89 @@ class Boids(no.Model):
     nearest = np.where(mindists == d2, True, False)
 
     too_close = np.where(d2 < self.exclusion**2, 1.0, 0.0)
-    nearest_in_range = np.logical_and(nearest, too_close).astype(float)
+    # nearest_in_range = np.logical_and(nearest, too_close).astype(float)
 
-    self.__separate(nearest_in_range, dx, dy, self.sep_max_turn)
+    self.__separate(too_close, dx, dy)
 
-    # this masks the align/cohere steps for boids that need to separate
-    mask = np.repeat(1.0 - np.sum(nearest_in_range, axis=0), self.N).reshape(self.N, self.N)
+    in_range = np.logical_and(
+      d2 < self.vision ** 2,
+      d2 > self.exclusion ** 2
+    ).astype(float)
 
-    in_range = np.logical_and(np.where(d2 < self.vision**2, 1.0, 0.0), mask).astype(float)
-    self.__cohere(in_range, dx, dy, self.cohere_max_turn)
-    self.__align(in_range, self.align_max_turn)
+    # in_range = np.logical_and(np.where(d2 < self.vision**2, 1.0, 0.0), mask).astype(float)
+    # in_range = np.where(d2 < self.vision**2, 1.0, 0.0)
+    self.__cohere(in_range, dx, dy)
+    self.__align(in_range)
+
+    self.__normalise_v()
 
     (self.boids.x, self.boids.y), (vx, vy) = self.domain.move((self.boids.x, self.boids.y),
-                                                              (np.cos(self.boids.theta) * self.speed, np.sin(self.boids.theta) * self.speed),
+                                                              (self.boids.vx * self.speed, self.boids.vy * self.speed),
                                                               self.timeline.dt(), ungroup=True)
 
+    sleep(0.001)
     self.__update_visualisation()
     # if self.timeline.index() > 300:
     #   self.halt()
 
-  def __align(self, in_range: np.ndarray, max_turn: float) -> None:
+  def __align(self, in_range: np.ndarray) -> None:
 
     weights = 1.0 / np.sum(in_range, axis=0)
     weights[weights == np.inf] = 0.0
 
-    # need to convert to x,y to average angles correctly
-    # matrix x vector <piecewise-x> vector = vector
-    mean_vx = in_range @ np.cos(self.boids.theta) * weights
-    mean_vy = in_range @ np.sin(self.boids.theta) * weights
-    # print(mean_vx, mean_vy)
-    delta = np.clip(np.arctan2(mean_vy, mean_vx) - self.boids.theta, -max_turn, max_turn)
-    self.boids.theta = (self.boids.theta + delta) % TWO_PI
+    mean_vx = (in_range * self.boids.vx.values) @ weights
+    mean_vy = (in_range * self.boids.vy.values) @ weights
 
-  def __cohere(self, in_range: np.ndarray, dx: np.ndarray, dy: np.ndarray, max_turn: float) -> None:
+    self.boids.vx -= mean_vx * Boids.ALIGN_COEFF
+    self.boids.vy -= mean_vy * Boids.ALIGN_COEFF
+
+
+  def __cohere(self, in_range: np.ndarray, dx: np.ndarray, dy: np.ndarray) -> None:
     weights = 1.0 / np.sum(in_range, axis=0)
     weights[weights == np.inf] = 0.0
-    # compute vectors to cen  tre of gravity of neighbours (if any), relative to boid location
-    # matrix * matrix * vector = vector
-    xbar = in_range @ dx @ weights
-    ybar = in_range @ dy @ weights
-    delta = np.clip(np.arctan2(ybar, xbar) - self.boids.theta, -max_turn, max_turn)
-    self.boids.theta = (self.boids.theta + delta) % TWO_PI
+    x = (in_range * dx) @ weights
+    y = (in_range * dy) @ weights
 
-  def __separate(self, in_range: np.ndarray, dx: np.ndarray, dy: np.ndarray, max_turn: float) -> None:
+    self.boids.vx -= x * Boids.COHERE_COEFF
+    self.boids.vy -= y * Boids.COHERE_COEFF
+
+  def __separate(self, in_range: np.ndarray, dx: np.ndarray, dy: np.ndarray) -> None:
+    # print(in_range @ dx)
+    # print(in_range @ dy)
+    # stop
     if np.all(in_range == 0):
       return
-    weights = 1.0 / np.sum(in_range, axis=0)
-    weights[weights == np.inf] = 0.0
+    # weights = 1.0 / np.sum(in_range, axis=0)
+    # weights[weights == np.inf] = 0.0
 
-    x = in_range @ dx @ weights
-    y = in_range @ dy @ weights
-    delta = np.clip(np.arctan2(y, x) - self.boids.theta, -max_turn, max_turn)
-    self.boids.theta = (self.boids.theta - delta) % TWO_PI
+    x = (in_range * dx).sum(axis=0) # @ weights
+    y = (in_range * dy).sum(axis=0) # @ weights
+
+    # print(self.boids)
+    # print(in_range)
+    # print(weights)
+    # print(dx)
+    # print(dy)
+    # print(x)
+    # print(y)
+
+    self.boids.vx += x * Boids.SEPARATE_COEFF
+    self.boids.vy += y * Boids.SEPARATE_COEFF
+    # print(self.boids)
+    # stop
+    # delta = np.clip(np.arctan2(y, x) - self.boids.theta, -max_turn, max_turn)
+    # self.boids.theta = (self.boids.theta - delta) % TWO_PI
+
+  def __normalise_v(self) -> None:
+    norm = np.clip(np.sqrt(self.boids.vx ** 2 + self.boids.vy ** 2), a_min = 0.00001, a_max=None)
+    self.boids.vx /= norm
+    self.boids.vy /= norm
 
   def __init_visualisation(self) -> tuple[Any, Any]:
     plt.ion()
 
     fig = plt.figure(constrained_layout=True, figsize=(8, 8))
-    g = plt.quiver(self.boids.x, self.boids.y, np.cos(self.boids.theta), np.sin(self.boids.theta), scale=50, width=0.005, headwidth=2)
+    g = plt.quiver(self.boids.x, self.boids.y, self.boids.vx, self.boids.vy, scale=50, width=0.005, headwidth=2)
     plt.xlim(0.0, self.range)
     plt.ylim(0.0, self.range)
     plt.axis("off")
@@ -121,6 +185,6 @@ class Boids(no.Model):
   def __update_visualisation(self) -> None:
 
     self.g.set_offsets(np.c_[self.boids.x, self.boids.y])
-    self.g.set_UVC(np.cos(self.boids.theta), np.sin(self.boids.theta))
+    self.g.set_UVC(self.boids.vx, self.boids.vy)
     # plt.savefig("/tmp/boids2d%04d.png" % self.timeline.index(), dpi=80)
     self.fig.canvas.flush_events()
--- examples/boids/model.py
+++ examples/boids/model.py
@@ -4,10 +4,10 @@ from boids2d import Boids
 
 N = 500 # number of boids
 range = 1.0 # extent of the domain
-vision = 0.2 # distance boids "see"
-exclusion = 0.02 # distance collision avoidance kicks in
+vision = 0.3 # distance boids "see"
+exclusion = 0.05 # distance collision avoidance kicks in
 
-speed = 0.5 # speed of movement
+speed = 1.0 # speed of movement
 
 m = Boids(N, range, vision, exclusion, speed)
 
